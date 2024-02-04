32c70063386ce9d9328d3cd2fac9fa69c5caed0b
th3fr3dy <taboutsafredy@gmail.com>
2024-02-04 14:05:02 +0100

    Upadte for write decompressed file into current directory ;)

--- src/modules/writeDecompressedFile.js
+++ src/modules/writeDecompressedFile.js
@@ -14,8 +14,8 @@ async function writeDecompressedFile (decodedText, inputCompressedPath) {
     try {
         let  getOriginalCompressedFileName = path.basename(inputCompressedPath, path.extname(inputCompressedPath))
         getOriginalCompressedFileName = getOriginalCompressedFileName.slice(0, -11)
-        const outputPath = path.resolve(process.cwd(), `${getOriginalCompressedFileName}_decompressed.txt`)
-        await fs.writeFile (outputPath, decodedText)
+        const outputDecompressedPath = path.resolve(process.cwd(), `${getOriginalCompressedFileName}_decompressed.txt`)
+        await fs.writeFile (outputDecompressedPath, decodedText)
     } catch (error) {
         console.error(`🚨 Error in writeDecompressedFile :\n ${error}`)
     }
