2f1222319a9565521ca99ad3a1fb6d014a24c843
th3fr3dy <taboutsafredy@gmail.com>
2024-02-22 11:03:04 +0100

    Update README.md documentation.

--- test/prefixCodeTable.test.js
+++ test/prefixCodeTable.test.js
@@ -1,9 +1,7 @@
 const prefixCodeTable = require("../src/prefixCodeTable")
 
-test("Get prefix code for each char !!", async () => {
+test("Get prefix code for each char !!", async (testFilePath) => {
     
-    const testFilePath = "C:\\Users\\user\\Desktop\\go-CodingChallenges\\HuffmanCompressionTool\\examples\\text.tester.txt"
-
     const prefixCodes = await prefixCodeTable(testFilePath)
     expect(prefixCodes.t).toEqual("000")
     expect(prefixCodes.e).toEqual("011")
